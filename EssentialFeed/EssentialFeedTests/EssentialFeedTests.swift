a1171f0d4ff57770c700cf53ce128e956f980993
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-05-24 22:58:07 -0700

    added dynamic URL to FeedLoader

--- EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeed/EssentialFeedTests/EssentialFeedTests.swift
@@ -9,13 +9,15 @@ import XCTest
 
 class RemoteFeedLoader {
     let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
     
     func load() {
-        client.get(from: URL(string: "https://www.google.com")!)
+        client.get(from: url)
     }
 }
 
@@ -34,19 +36,21 @@ class MockHTTPClient: HTTPClient {
 class EssentialFeedTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://www.google.com")!
         let client = MockHTTPClient()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
+        let url = URL(string: "https://www.google.com")!
         let client = MockHTTPClient()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
         
         sut.load()
         
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(url, client.requestedURL)
     }
 
 }
