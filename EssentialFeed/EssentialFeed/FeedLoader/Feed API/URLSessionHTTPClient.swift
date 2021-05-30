446da437001e002d47dcf673fc645027fd13fa2e
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-05-30 15:56:05 -0700

    moved URLSessionHTTPClient to its own file in production

---
+++ EssentialFeed/EssentialFeed/FeedLoader/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Kenneth Dubroff on 5/30/21.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    /// error for invalid scenarios
+    private struct UnexpectedValueRepresentation: Error {}
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data,
+                      let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValueRepresentation()))
+            }
+        }.resume()
+    }
+}
