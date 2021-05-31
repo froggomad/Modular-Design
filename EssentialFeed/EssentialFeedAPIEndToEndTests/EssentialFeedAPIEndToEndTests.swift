27ed4e2ac1a666324d72a17399feed44174965da
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-05-31 00:16:51 -0700

    checking for retain cycles

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -35,11 +35,12 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     // MARK: - Helpers
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
-
+        assertNoMemoryLeak(client, file: file, line: line)
+        assertNoMemoryLeak(loader, file: file, line: line)
         let exp = expectation(description: "Wait for load completion")
 
         var receivedResult: LoadFeedResult?
