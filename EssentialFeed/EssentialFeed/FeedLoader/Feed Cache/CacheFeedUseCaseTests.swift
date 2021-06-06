37f8f13afa6a5c34ee6b6ee94e7a1fbd10468410
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-06-06 09:06:46 -0700

    Ensured saving requests deletion from the cache

--- EssentialFeed/EssentialFeed/FeedLoader/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeed/FeedLoader/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+    private let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -24,6 +35,29 @@ class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItem]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    private var uniqueItem: FeedItem {
+        FeedItem(id: UUID(),
+                 description: "Unique Test Item",
+                 location: "any-location",
+                 imageURL: anyURL
+        )
+    }
+    
+    private var anyURL: URL {
+        URL(string: "http://any-url.com")!
     }
     
 }
