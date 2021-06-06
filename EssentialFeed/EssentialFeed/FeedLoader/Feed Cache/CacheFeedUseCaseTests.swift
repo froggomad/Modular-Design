998b32e9010dc4a8358edf47a8b54a3c5e9f2a3c
Kenny Dubroff (froggomad) <kenny.dubroff@kennydubroff.com>
2021-06-06 08:20:07 -0700

    Ensured `LocalFeedLoader` instantiation doesn't delete cache

---
+++ EssentialFeed/EssentialFeed/FeedLoader/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeed
+//
+//  Created by Kenneth Dubroff on 6/6/21.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+}
